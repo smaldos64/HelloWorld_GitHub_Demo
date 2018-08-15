e9270b52de7bcb5324dbc713c26235224f8c13cf
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-08-15 10:42:06 +0200

    Ryddet op !!!!

---
+++ HelloWorld_GitHub_Demo/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_GitHub_Demo
+{
+    public class Point
+    {
+        public double X_Value;
+        public double Y_Value;
+    }
+}
