e246363f348887966a59a9945617fa65c252c1e8
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-08-15 10:48:54 +0200

    Nu kommer Point3Dimension med. Fordi den nu er i "staged" tilstand.

---
+++ HelloWorld_GitHub_Demo/Point3Dimension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_GitHub_Demo
+{
+    class Point3Dimension : Point
+    {
+        double Z_Value;
+    }
+}
