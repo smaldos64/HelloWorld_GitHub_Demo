705f5ad959145c03a5337663922756be8f872bb9
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-08-15 10:58:06 +0200

    Udskrift af 3 dimensionelt punkt i Program.cs

--- HelloWorld_GitHub_Demo/Program.cs
+++ HelloWorld_GitHub_Demo/Program.cs
@@ -10,10 +10,17 @@ namespace HelloWorld_GitHub_Demo
     {
         static void Main(string[] args)
         {
+            Point3Dimension My3DimensionPoint = new Point3Dimension();
             Console.WriteLine("1");
             Console.WriteLine("2");
             Console.WriteLine("3");
             Console.WriteLine("5");
+
+            My3DimensionPoint.X_Value = 1;
+            My3DimensionPoint.Y_Value = 2;
+            My3DimensionPoint.Z_Value = 3;
+            Console.WriteLine("Punktets koordinater er : ({0} ; {1} ; {2})",
+                My3DimensionPoint.X_Value, My3DimensionPoint.Y_Value, My3DimensionPoint.Z_Value);
         }
     }
 }
