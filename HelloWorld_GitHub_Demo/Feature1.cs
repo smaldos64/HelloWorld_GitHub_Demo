28c4506c5896919add021aa1d1d36acddf7238d4
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-08-17 13:57:42 +0200

    Added Feature 1

---
+++ HelloWorld_GitHub_Demo/Feature1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_GitHub_Demo
+{
+    public class Feature1
+    {
+    }
+}
